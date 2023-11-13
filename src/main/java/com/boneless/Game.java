7d99c349fde7abd45948c2ce4ca3976e75ca2307
Boneless-Sans <bonelessmail0101@gmail.com>
2023-11-13 13:23:07 -0800

    pain

--- src/main/java/com/boneless/Game.java
+++ src/main/java/com/boneless/Game.java
@@ -4,6 +4,7 @@ import com.boneless.util.JsonFile;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Game {
     private String fileName = "main_board.json";
@@ -20,22 +21,27 @@ public class Game {
         frame.setLayout(new BorderLayout());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        JPanel title = new JPanel(new FlowLayout());
+        JLabel titleText = new JLabel(JsonFile.read(getFileName(), "data", "title"));
+        titleText.setFont(new Font(JsonFile.read(getFileName(), "data", "font_name"), Font.PLAIN, Integer.parseInt(JsonFile.read(getFileName(), "data", "font_size"))));
+        title.add(titleText);
+
         JPanel gameBoard = new JPanel(new GridLayout());
         gameBoard.setPreferredSize(new Dimension(0,300));
         gameBoard.setBackground(Color.BLUE);
 
         JPanel teams = createTeamsPanel();
 
-        frame.add(createBoard(fileName), BorderLayout.CENTER);
+        frame.add(title, BorderLayout.NORTH);
+        frame.add(createBoard(getFileName()), BorderLayout.CENTER);
         frame.add(teams, BorderLayout.SOUTH);
         frame.setVisible(true);
     }
     private JPanel createBoard(String fileName) {
         JPanel gameBoard = new JPanel();
-        //gameBoard.setPreferredSize(new Dimension(0,300));
-        //gameBoard.setBackground();
-        int sizeX = Integer.parseInt(JsonFile.read(getFileName(), "numColumns", "count"));
-        int sizeY = Integer.parseInt(JsonFile.read(getFileName(), "numRows", "count"));
+
+        int sizeX = Integer.parseInt(JsonFile.read(fileName, "data", "columns"));
+        int sizeY = Integer.parseInt(JsonFile.read(fileName, "data", "rows"));
 
         System.out.println("\n" + sizeX + "\n" + sizeY + "\n");
 
@@ -47,17 +53,45 @@ public class Game {
 //            gameBoard.add(button);
 //        }
 
-        gameBoard.add(createButton(fileName, sizeX, sizeY));
+        JButton[] buttons = createTitles(fileName, sizeX, sizeY);
+        JButton[] rowOne = createRows(fileName, sizeX, sizeY);
+        for (JButton button : buttons) {
+            gameBoard.add(button);
+        }
+        for(JButton button : rowOne){
+            gameBoard.add(button);
+        }
         return gameBoard;
     }
-    private JButton createButton(String filename, int maxColumn, int maxRow){
-        JButton button = new JButton();
-        for(int i = 1; i <= maxColumn; i++){
-            button = new JButton(JsonFile.readWithThreeKeys(filename, "column_1", "questions", "row_" + i));
+    public JButton[] createTitles(String filename, int sizeX, int sizeY) {
+        //it adds left to right, so there will need to be some math to calculate when to add titles and questions
+
+        String[] titles = new String[sizeX]; // Declare the array of sizeX elements
+
+        for (int i = 1; i <= sizeX; i++) {
+            titles[i-1] = JsonFile.read(filename, "column_" + i, "title"); // Read the title and store it in the array
         }
-        System.out.println(JsonFile.readWithThreeKeys(filename, "column_1", "questions", "row_1"));
-        return button;
+        //draw each line via their row number
+        JButton[] buttons = new JButton[sizeX];
+        for(int i = 0; i < sizeX; i++){
+            buttons[i] = new JButton(titles[i]);
+        }
+        return buttons;
     }
+    public JButton[] createRows(String filename, int sizeX, int sizeY){
+        String[] rowOne = new String[sizeX];
+        for(int i = 1; i <= sizeX; i++){
+            rowOne[i-1] = JsonFile.readWithThreeKeys(filename, "column_" + i, "questions","row_" + i);
+        }
+
+        JButton[] buttons = new JButton[sizeX];
+        for(int i = 0; i < sizeX; i++){
+            buttons[i] = new JButton(rowOne[i]);
+        }
+        return buttons;
+    }
+
+
     private JPanel createTeamsPanel(){
         JPanel teams = new JPanel();
         teams.setPreferredSize(new Dimension(0,100));
--- src/main/java/com/boneless/saveexample.java
+++ src/main/java/com/boneless/saveexample.java
@@ -1,55 +1,17 @@
 package com.boneless;
 
-import com.sun.tools.javac.Main;
-import java.util.Scanner;
-
 import com.boneless.util.JsonFile;
 
-import java.io.IOException;
-import java.util.Scanner;
-
-public class saveexample {
-    private String teamName;
-    private int score;
-    private int playerCount;
-
+public class saveexample{
     public static void main(String[] args){
-        Scanner scan = new Scanner(System.in);
+        int sizeX = 6;
+        String filename = "main_board.json";
+        String[] titles = new String[sizeX]; // Declare the array of sizeX elements
 
-        System.out.println("What color\nBackground (1), Buttons (2), Foreground (3)");
-
-        switch (scan.nextInt()){
-            case 1:
-                String backgroundColor = getScan();
-                saveColor("background", backgroundColor);
-                break;
-            case 2:
-                String buttonColor = getScan();
-                saveColor("button", buttonColor);
-                break;
-            case 3:
-                String foregroundColor = getScan();
-                saveColor("foreground", foregroundColor);
-                break;
-            default:
-                System.out.println("Invalid choice dumbass");
-                break;
+        for (int i = 1; i <= sizeX; i++) {
+            titles[i-1] = JsonFile.read(filename, "column_" + i, "title"); // Read the title and store it in the array
+            System.out.println(titles[i -1]);
         }
-        scan.close();
-        System.out.println(
-                JsonFile.read("settings.json", "colors", "background") + "\n" +
-                        JsonFile.read("settings.json", "colors", "button") + "\n" +
-                        JsonFile.read("settings.json", "colors", "foreground") + "\n"
-        );
-    }
-    private static String getScan(){
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Color");
 
-        return scan.next();
-    }
-
-    private static void saveColor(String key, String color){
-        JsonFile.writeln("settings.json", "colors", key, color);
     }
 }
