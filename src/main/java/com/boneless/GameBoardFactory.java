b42766bead37e4100e34780ea88c123a7e6ee1ed
Boneless-Sans <bonelessmail0101@gmail.com>
2023-12-21 11:34:37 -0800

    zamn i should sketch

--- src/main/java/com/boneless/GameBoardFactory.java
+++ src/main/java/com/boneless/GameBoardFactory.java
@@ -5,7 +5,17 @@ import javax.swing.*;
 import static com.boneless.Launcher.changeButtonState;
 
 public class GameBoardFactory extends JFrame {
+    private String title;
+    private int columns;
+    private int rows;
     public GameBoardFactory(){
+        initUI();
+    }
+    private void initUI(){
+        setSize(1280, 720);
+        setLocationRelativeTo(null);
 
+
+        setVisible(true);
     }
 }
